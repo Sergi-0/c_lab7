ae99580a38f43a9dc47b075c8b5e1f60806e559f
Sergi-0 <144801688+Sergi-0@users.noreply.github.com>
2024-12-04 21:20:26 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,93 @@
+﻿namespace ClassLibrary
+{
+    public class MyAttribute : Attribute
+    {
+        public string Comment { get; set; }
+        MyAttribute() { }
+        public MyAttribute(string Comment)
+        {
+            this.Comment = Comment;
+        }
+    }
+
+    [MyAttribute("Классификация животных(травоядные, хищники, всеядные")]
+    public enum eClassificationAnimal
+    {
+        Herbivores,
+        Carnivores,
+        Omnivores
+    }
+
+    [MyAttribute("Классификация еды(мясо, растения, всё")]
+    public enum eFavouriteFood
+    {
+        Meat,
+        Plants,
+        Everything
+    }
+
+    [MyAttribute("Родительский класс Animal ")]
+    public abstract class Animal
+    {
+        public string Country { get; set; } // обязательно должны быть именно свойства(properties), а не поля !
+        public string HideFromOtherAnimals {  get; set; }
+        public string Name {  get; set; }
+        public string WhatAnimal {  get; set; }
+        public eClassificationAnimal classifications {  get; set; }
+        public eFavouriteFood favouriteFood {  get; set; }
+
+        public Animal(string Country, string HideFromOtherAnimals, string Name, string WhatAnimal, eClassificationAnimal classifications, eFavouriteFood favouriteFood)
+        {
+            this.Country = Country;
+            this.HideFromOtherAnimals = HideFromOtherAnimals;
+            this.Name = Name;
+            this.WhatAnimal = WhatAnimal;
+            this.classifications = classifications;
+            this.favouriteFood = favouriteFood;
+        }
+
+        public void Deconstruct(out string Country, out string HideFromOtherAnimals, out string Name, out string WhatAnimal)
+        {
+            Country = this.Country;
+            HideFromOtherAnimals = this.HideFromOtherAnimals;
+            Name = this.Name;
+            WhatAnimal = this.WhatAnimal;
+        }
+
+        public eClassificationAnimal GetClassificationsAnimal() { return classifications; }
+
+        public virtual string GetFavouriteFood() { return favouriteFood.ToString(); }
+
+        public abstract void SayHello();
+    }
+
+    [MyAttribute("Класс коровы производный от Animal")]
+    public class Cow : Animal
+    {
+        public Cow(string Country, string HideFromOtherAnimals, string Name, string WhatAnimal, eClassificationAnimal classifications, eFavouriteFood favouriteFood) : base(Country, HideFromOtherAnimals, Name, WhatAnimal, classifications, favouriteFood) { }
+
+        public override string GetFavouriteFood() { return $"Корова любит {favouriteFood}. Ням-ням!"; }
+
+        public override void SayHello() { Console.WriteLine("ММУУУУУУУУУУУ!!! Hello"); }
+    }
+
+    [MyAttribute("Класс льва производный от Animal")]
+    public class Lion : Animal
+    {
+        public Lion(string Country, string HideFromOtherAnimals, string Name, string WhatAnimal, eClassificationAnimal classifications, eFavouriteFood favouriteFood) : base(Country, HideFromOtherAnimals, Name, WhatAnimal, classifications, favouriteFood) { }
+
+        public override string GetFavouriteFood() { return $"Лев любит {favouriteFood}. РРНям-ням!"; }
+
+        public override void SayHello() { Console.WriteLine("РРРРРРРРРРРРрр!!!"); }
+    }
+
+    [MyAttribute("Класс свиньи производный от Animal")]
+    public class Pig : Animal
+    {
+        public Pig(string Country, string HideFromOtherAnimals, string Name, string WhatAnimal, eClassificationAnimal classifications, eFavouriteFood favouriteFood) : base(Country, HideFromOtherAnimals, Name, WhatAnimal, classifications, favouriteFood) { }
+
+        public override string GetFavouriteFood() { return $"Свинья любит {favouriteFood}. Хр.., ой то есть, Ням-ням!"; }
+
+        public override void SayHello() { Console.WriteLine("ХРЮюююю ХРЮЮЮЮЮ!!!"); }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Xml;
+using ClassLibrary;
+
+namespace lab7_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string OutputFileName = "ClassDiagram.xml"; // имя выходного файла
+            XmlDocument xmlDoc = new XmlDocument(); // создаем документ xml формата
+            XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", "UTF-8", null); // версия и кодировка
+            xmlDoc.AppendChild(xmlDeclaration); // добавляем описание в документ
+            XmlElement HeadElement = xmlDoc.CreateElement("ClassDiagram"); // создаем элемент xml формата, который позже добавим в документ
+            xmlDoc.AppendChild(HeadElement); // добавляем элемент в документ
+            Assembly Assembly = Assembly.Load("ClassLibraryLAB7"); // загрузку сборки (библиотеки)
+            Type[] types = Assembly.GetTypes(); // получение всех типов сборки
+
+            foreach (Type type in types)
+            {
+                XmlElement ClassElement = xmlDoc.CreateElement("Class");
+                ClassElement.SetAttribute("name", type.Name);
+                object[] attributes = type.GetCustomAttributes(typeof(MyAttribute), false); // false - отвечает за то чтобы, брались аттрибуты именно нашего класса, не включая родительский
+                if (attributes.Length > 0)
+                {
+                    MyAttribute СustomAttribute = (MyAttribute)attributes[0];
+                    ClassElement.SetAttribute("Comment", СustomAttribute.Comment);
+                }
+
+                PropertyInfo[] Properties = type.GetProperties(); // получаем свойства данного класса
+                foreach (PropertyInfo property in Properties)
+                {
+                    XmlElement PropertyElement = xmlDoc.CreateElement("property");
+                    PropertyElement.SetAttribute("name", property.Name);
+                    PropertyElement.SetAttribute("type", property.PropertyType.Name);
+                    ClassElement.AppendChild(PropertyElement);
+                }
+                HeadElement.AppendChild(ClassElement);
+            }
+            xmlDoc.Save(OutputFileName);
+        }
+    }
+}
